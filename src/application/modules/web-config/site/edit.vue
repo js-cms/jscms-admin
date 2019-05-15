2dec66864b978664cae6598cbf08f02e51815b96
lisniuse <17560235@qq.com>
2019-05-15 23:56:06 +0800

    feat: new fields for config of site.

--- src/application/modules/web-config/site/edit.vue
+++ src/application/modules/web-config/site/edit.vue
@@ -8,61 +8,126 @@
         </span>
       </div>
       <div class="h-panel-body">
-        <!-- <jscms-form :form="form" :parent="this" :labelWidth="130"></jscms-form> -->
 
-        <!-- 站点主要信息 -->
+        <!-- 元信息设置 -->
         <div class="main-block-warp" v-if="form">
           <div class="h-panel">
             <div class="h-panel-bar">
-              <span class="h-panel-title">站点主要信息</span>
+              <span class="h-panel-title">元信息设置</span>
             </div>
             <div class="h-panel-body">
               <Form :label-width="110">
-                <FormItem label="分类的封面">
-                  <upload-avatar
-                    buttonName="上传图片"
-                    :action="uploadAction"
-                    :extraData="uploadData"
-                    :imageUrl="form.fields.poster.value"
-                    :height="198"
-                    :width="300"
-                    @complete="(type, res) => {
-                      if (type === 'success') {
-                        $Message[res.code === 0 ? 'success' : 'error'](res.msg);
-                        form.fields.poster.value = res.data.imageUrl;
-                      } else {
-                        $Message.error('未知错误');
-                      }
-                    }" 
-                  ></upload-avatar>
+                <FormItem label="站点首页标题">
+                  <input type="text" v-model="form.fields.title.value" :placeholder="form.fields.title.placeholder">
                 </FormItem>
-                <FormItem label="中文分类名称">
-                  <input type="text" v-model="form.fields.name.value" :placeholder="form.fields.name.placeholder">
+                <FormItem label="站点标题分割符">
+                  <input type="text" v-model="form.fields.separator.value" :placeholder="form.fields.separator.placeholder">
                 </FormItem>
-                <FormItem label="英文分类别名">
-                  <input type="text" v-model="form.fields.alias.value" :placeholder="form.fields.alias.placeholder">
+                <FormItem label="站点副标题">
+                  <input type="text" v-model="form.fields.subtitle.value" :placeholder="form.fields.subtitle.placeholder">
                 </FormItem>
-                <FormItem label="分类标签">
-                  <TagInput
-                    v-model="form.fields.tags.value"
-                    :limit="20"
-                    :wordlimit="20"
-                    placeholder="输入标签，回车键确认"
-                  ></TagInput>
+                <FormItem label="站点首页关键字">
+                  <input type="text" v-model="form.fields.keywords.value" :placeholder="form.fields.keywords.placeholder">
                 </FormItem>
-                <FormItem label="分类简介">
-                  <textarea
-                    :name="form.fields.intro.name"
-                    v-model="form.fields.intro.value"
-                    :rows="form.fields.intro.extra.rows || 6"
-                    :placeholder="form.fields.intro.placeholder"
-                  ></textarea>
+                <FormItem label="站点图标地址">
+                  <input type="text" v-model="form.fields.favicon.value" :placeholder="form.fields.favicon.placeholder">
+                </FormItem>
+                <FormItem label="站点描述">
+                  <textarea v-model="form.fields.description.value" :rows="6" :placeholder="form.fields.description.placeholder"></textarea>
                 </FormItem>
               </Form>
             </div>
           </div>
         </div>
 
+        <!-- 基本信息设置 -->
+        <div class="main-block-warp" v-if="form">
+          <div class="h-panel">
+            <div class="h-panel-bar">
+              <span class="h-panel-title">基本信息设置</span>
+            </div>
+            <div class="h-panel-body">
+              <Form :label-width="130">
+                <FormItem label="站点logo地址">
+                  <input type="text" v-model="form.fields.logo.value" :placeholder="form.fields.logo.placeholder">
+                </FormItem>
+                <FormItem label="icp备案信息">
+                  <input type="text" v-model="form.fields.icp.value" :placeholder="form.fields.icp.placeholder">
+                </FormItem>
+                <FormItem label="微博地址">
+                  <input type="text" v-model="form.fields.weibo.value" :placeholder="form.fields.weibo.placeholder">
+                </FormItem>
+                <FormItem label="邮箱地址">
+                  <input type="text" v-model="form.fields.email.value" :placeholder="form.fields.email.placeholder">
+                </FormItem>
+                <FormItem label="qq号">
+                  <input type="text" v-model="form.fields.qq.value" :placeholder="form.fields.qq.placeholder">
+                </FormItem>
+                <FormItem label="电话号码">
+                  <input type="text" v-model="form.fields.phone.value" :placeholder="form.fields.phone.placeholder">
+                </FormItem>
+                <FormItem label="微信公号二维码">
+                  <input type="text" v-model="form.fields.vxqr.value" :placeholder="form.fields.vxqr.placeholder">
+                </FormItem>
+                <FormItem label="微信收款二维码">
+                  <input type="text" v-model="form.fields.wxpay.value" :placeholder="form.fields.wxpay.placeholder">
+                </FormItem>
+                <FormItem label="支付宝收款二维码">
+                  <input type="text" v-model="form.fields.alipay.value" :placeholder="form.fields.alipay.placeholder">
+                </FormItem>
+                <FormItem label="关于网站">
+                  <textarea type="text" v-model="form.fields.about.value" :rows="6" :placeholder="form.fields.about.placeholder"></textarea>
+                </FormItem>
+                <FormItem label="文章页声明">
+                  <CodeEditor v-model="form.fields.articleStatement.value" :height="form.fields.articleStatement.extra.height" :mode="form.fields.articleStatement.extra.lang||''"  />
+                </FormItem>
+                <FormItem label="网站底部代码">
+                  <CodeEditor v-model="form.fields.footHTML.value" :height="form.fields.footHTML.extra.height" :mode="form.fields.footHTML.extra.lang||''"  />
+                </FormItem>
+              </Form>
+            </div>
+          </div>
+        </div>
+
+        <!-- 开关配置信息 -->
+        <div class="main-block-warp" v-if="form">
+          <div class="h-panel">
+            <div class="h-panel-bar">
+              <span class="h-panel-title">开关配置信息</span>
+            </div>
+            <div class="h-panel-body">
+              <Form :label-width="150">
+                <FormItem label="开启前台注册登录">
+                  <Radio v-model="form.fields.boolWebReg.value" :datas="[{val: true, name: '开启'}, {val: false, name: '关闭'}]" keyName="val" titleName="name"></Radio>
+                </FormItem>
+                <FormItem label="开启邮箱注册登录">
+                  <Radio v-model="form.fields.boolWebRegEmail.value" :datas="[{val: true, name: '开启'}, {val: false, name: '关闭'}]" keyName="val" titleName="name"></Radio>
+                </FormItem>
+                <FormItem label="开启邮箱注册激活">
+                  <Radio v-model="form.fields.boolWebRegEmailActive.value" :datas="[{val: true, name: '开启'}, {val: false, name: '关闭'}]" keyName="val" titleName="name"></Radio>
+                </FormItem> 
+                <FormItem label="开启手机注册登录">
+                  <Radio v-model="form.fields.boolWebRegPhone.value" :datas="[{val: true, name: '开启'}, {val: false, name: '关闭'}]" keyName="val" titleName="name"></Radio>
+                </FormItem>
+                <FormItem label="开启登录才能评论">
+                  <Radio v-model="form.fields.boolCommentLogin.value" :datas="[{val: true, name: '开启'}, {val: false, name: '关闭'}]" keyName="val" titleName="name"></Radio>
+                </FormItem>
+                <FormItem label="开启注册登录验证码">
+                  <Radio v-model="form.fields.boolLoginVercode.value" :datas="[{val: true, name: '开启'}, {val: false, name: '关闭'}]" keyName="val" titleName="name"></Radio>
+                </FormItem>
+                <FormItem label="邮箱服务器主机">
+                  <input type="text" v-model="form.fields.emailConfService.value" :placeholder="form.fields.emailConfService.placeholder">
+                </FormItem>
+                <FormItem label="邮箱服务账号">
+                  <input type="text" v-model="form.fields.emailConfUser.value" :placeholder="form.fields.emailConfUser.placeholder">
+                </FormItem>
+                <FormItem label="邮箱服务密码">
+                  <input type="text" v-model="form.fields.emailConfPass.value" :placeholder="form.fields.emailConfPass.placeholder">
+                </FormItem>
+              </Form>
+            </div>
+          </div>
+        </div>
         <Loading text="Loading" :loading="containerLoading"></Loading>
       </div>
     </div>
@@ -144,6 +209,10 @@ export default {
 .basic-form-vue {
   .h-panel-body {
     padding-right: 60px;
+  }
+
+  .main-block-warp {
+    margin-bottom: 15px;
   }
 }
 </style>
--- src/application/modules/web-config/site/model/Site.js
+++ src/application/modules/web-config/site/model/Site.js
@@ -6,26 +6,38 @@ let model = new modelman.Model({
 });
 
 model.assign({
-  title: { n: '标题元信息', type: 'String', f: true, t: true, r: true }, //首页标题
-  subtitle: { n: '副标题元信息', type: 'String', f: true, t: true, r: true }, //副标题
-  keywords: { n: '关键字元信息', type: 'String', f: true, t: true, r: true }, //首页关键字
-  separator: { n: '标题分割符', type: 'String', f: true, t: false, r: true }, //标题分割符
-  icp: { n: 'icp备案信息', type: 'String', f: true, t: false, r: false }, //icp备案信息
-  logo: { n: '站点logo地址', type: 'String', f: true, t: false, r: false }, //站点logo地址
-  favicon: { n: '站点图标地址', type: 'String', f: true, t: false, r: false }, //站点图标地址
-  description: { n: '描述元信息', type: 'String', f: true, t: true, r: false, extra: {comType: 'textarea'}}, //首页描述元信息
-  about: { n: '关于网站', type: 'String', f: true, t: true, r: false, extra: {comType: 'textarea'}}, //关于网站
-  articleStatement: { n: '文章页声明', type: 'String', f: true, t: false, r: false, extra: {comType: 'codeEdit', lang: 'html', height: '200px'}}, //关于网站
-  footHTML: { n: '网站底部代码', type: 'String', f: true, t: false, r: false, extra: {comType: 'codeEdit', lang: 'html', height: '200px'}}, //网站底部代码
-  boolWebReg: { n: '前台注册登录', type: 'Boolean', f: true, t: false, r: true, d: true, extra: {comType: 'radio', options: 'true:开放,false:关闭'}}, //开放前台注册
+  // 元信息设置
+  title: { n: '站点首页标题', type: 'String', f: true, t: true, r: true, p: '站点首页标题，一般不超过80个字符。' }, // 站点首页标题
+  separator: { n: '站点标题分割符', type: 'String', f: true, t: false, r: true, p: '站点标题分割符' }, // 站点标题分割符
+  subtitle: { n: '站点副标题', type: 'String', f: true, t: true, r: true, p: '站点副标题，位于标题分隔符后面。' }, // 站点副标题
+  keywords: { n: '站点首页关键字', type: 'String', f: true, t: true, r: true, p: '站点首页关键字，一般不超过100个字符。' }, // 站点首页关键字
+  favicon: { n: '站点图标地址', type: 'String', f: true, t: false, r: false, p: '请输入图标的网址'}, // 站点图标地址
+  description: { n: '站点描述', type: 'String', f: true, t: true, r: false, p: '站点描述，一般不超过200个字符。', extra: {comType: 'textarea'}}, //首页描述元信息
+
+  // 网站信息设置
+  logo: { n: '站点logo地址', type: 'String', f: true, t: false, r: false, p: '网站logo的图片网址'}, //站点logo地址
+  icp: { n: 'icp备案信息', type: 'String', f: true, t: false, r: false, p: '填写你在工信部的网址备案号，如果没有就留空' }, //icp备案信息
+  weibo: { n: '微博地址', type: 'String', f: true, t: true, r: false, p: '微博地址'}, //微博地址
+  email: { n: '邮箱地址', type: 'Email', f: true, t: true, r: false, p: '邮箱地址'}, //邮箱地址
+  qq: { n: 'qq号', type: 'String', f: true, t: true, r: false, p: 'qq号'}, //qq号
+  phone: { n: '电话号码', type: 'String', f: true, t: true, r: false, p: '电话号码'}, //电话号码
+  vxqr: { n: '微信公号二维码', type: 'String', f: true, t: true, r: false, p: '微信公众号二维码的地址'}, // 微信公众号二维码的地址
+  wxpay: { n: '微信收款二维码', type: 'String', f: true, t: true, r: false, p: '微信收款二维码'}, // 微信收款二维码
+  alipay: { n: '支付宝收款二维码', type: 'String', f: true, t: true, r: false, p: '支付宝收款二维码'}, // 支付宝收款二维码
+  about: { n: '关于网站', type: 'String', f: true, t: true, r: false, p: '关于网站，可填写网址的介绍，此文本用于展示。', extra: {comType: 'textarea'}}, //关于网站
+  articleStatement: { n: '文章页声明', type: 'String', f: true, t: false, p: '文章底部的声明，此文本用于展示，支持html代码', r: false, extra: {comType: 'codeEdit', lang: 'html', height: '300px'}}, //文章页声明
+  footHTML: { n: '网站底部代码', type: 'String', f: true, t: false, r: false, p: '网站地步代码，可以插入类似网站统计的js脚本。', extra: {comType: 'codeEdit', lang: 'html', height: '300px'}}, //网站底部代码
+
+  // 开关配置信息
+  boolWebReg: { n: '开启前台注册登录', type: 'Boolean', f: true, t: false, r: true, d: true, extra: {comType: 'radio', options: 'true:开放,false:关闭'}}, //开放前台注册
   boolWebRegEmail: { n: '邮箱注册登录', type: 'Boolean', f: true, t: false, r: true, d: true, extra: {comType: 'radio', options: 'true:开放,false:关闭'}}, //开放前台注册
   boolWebRegEmailActive: { n: '邮箱注册激活', type: 'Boolean', f: true, t: false, r: true, d: true, extra: {comType: 'radio', options: 'true:开放,false:关闭'}}, //开放前台注册
   boolWebRegPhone: { n: '手机注册登录', type: 'Boolean', f: true, t: false, r: true, d: false, extra: {comType: 'radio', options: 'true:开放,false:关闭'}}, //开放前台注册
   boolCommentLogin: { n: '登录才能评论', type: 'Boolean', f: true, t: false, r: true, d: false, extra: {comType: 'radio', options: 'true:必须登录,false:不必登录'}}, //必须登录.才能评论
   boolLoginVercode: { n: '注册登录验证码', type: 'Boolean', f: true, t: false, r: true, d: true, extra: {comType: 'radio', options: 'true:开启,false:关闭'}}, //开启登录注册验证码
-  emailConfService: { n: '邮箱服务器', type: 'String', f: true, t: false, r: false }, // 邮箱服务器
-  emailConfUser: { n: '邮箱服务账号', type: 'String', f: true, t: false, r: false }, // 邮箱服务账号
-  emailConfPass: { n: '邮箱服务密码', type: 'String', f: true, t: false, r: false } // 邮箱服务密码
+  emailConfService: { n: '邮箱服务器', type: 'String', f: true, t: false, r: false, p: '填写邮箱服务器主机' }, // 邮箱服务器
+  emailConfUser: { n: '邮箱服务账号', type: 'String', f: true, t: false, r: false, p: '填写邮箱服务账号'}, // 邮箱服务账号
+  emailConfPass: { n: '邮箱服务密码', type: 'String', f: true, t: false, r: false, p: '填写邮箱服务密码' } // 邮箱服务密码
 });
 
 export default model;
